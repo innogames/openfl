2b82b93ee27f7eff8c074a90b863b145a46568b3
nenad.bojkovski <nenadbojkovski@gmail.com>
2018-02-27 19:32:18 +0100

    allDirty reverted

--- src/openfl/display3D/Program3D.hx
+++ src/openfl/display3D/Program3D.hx
@@ -232,7 +232,7 @@ import openfl.Vector;
 			
 			for (uniform in __uniforms) {
 				
-				if (/*__allDirty || */uniform.isDirty) {
+				if (__allDirty || uniform.isDirty) {
 					
 					uniform.flush ();
 					uniform.isDirty = false;
