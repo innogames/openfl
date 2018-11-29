b9ce3cac743df9b2d11845c09a58a49939a34293
nenad.bojkovski <nenadbojkovski@gmail.com>
2018-11-29 16:32:08 +0100

    Remove unnecessary (commented) code

--- src/openfl/_internal/renderer/opengl/batcher/BlendMode.hx
+++ src/openfl/_internal/renderer/opengl/batcher/BlendMode.hx
@@ -15,16 +15,6 @@ class BlendMode {
 	public static var MASK(default,never) = new BlendMode(GL.FUNC_ADD, GL.ZERO, GL.SRC_ALPHA);
 	public static var BELOW(default,never) = new BlendMode(GL.FUNC_ADD, GL.ONE_MINUS_DST_ALPHA, GL.DST_ALPHA);
 
-	// public static var NOPREMULT_NONE(default,never) = new BlendMode(GL.FUNC_ADD, GL.ONE, GL.ZERO);
-	// public static var NOPREMULT_NORMAL(default,never) = new BlendMode(GL.FUNC_ADD, GL.ONE, GL.ONE_MINUS_SRC_ALPHA);
-	// public static var NOPREMULT_ADD(default,never) = new BlendMode(GL.FUNC_ADD, GL.ONE, GL.ONE);
-	// public static var NOPREMULT_MULTIPLY(default,never) = new BlendMode(GL.FUNC_ADD, GL.DST_COLOR, GL.ONE_MINUS_SRC_ALPHA);
-	// public static var NOPREMULT_SCREEN(default,never) = new BlendMode(GL.FUNC_ADD, GL.ONE, GL.ONE_MINUS_SRC_COLOR);
-	// public static var NOPREMULT_SUBTRACT(default,never) = new BlendMode(GL.FUNC_REVERSE_SUBTRACT, GL.ONE, GL.ONE);
-	// public static var NOPREMULT_ERASE(default,never) = new BlendMode(GL.FUNC_ADD, GL.ZERO, GL.ONE_MINUS_SRC_ALPHA);
-	// public static var NOPREMULT_MASK(default,never) = new BlendMode(GL.FUNC_ADD, GL.ZERO, GL.SRC_ALPHA);
-	// public static var NOPREMULT_BELOW(default,never) = new BlendMode(GL.FUNC_ADD, GL.ONE_MINUS_DST_ALPHA, GL.DST_ALPHA);
-
 	public static var NOPREMULT_NONE(default,never) = new BlendMode(GL.FUNC_ADD, GL.ONE, GL.ZERO);
 	public static var NOPREMULT_NORMAL(default,never) = new BlendMode(GL.FUNC_ADD, GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA);
 	public static var NOPREMULT_ADD(default,never) = new BlendMode(GL.FUNC_ADD, GL.SRC_ALPHA, GL.DST_ALPHA);
