24f3dd355afb81537d33eb30bb823cc064f33edb
Dan Korostelev <nadako@gmail.com>
2019-10-21 14:12:33 +0200

    cleanup lime clipboard
    - remove unused onUpdate signal
    - remove flash leftovers and other non-html5 stuff
    - inline get_text

--- src/lime/system/Clipboard.hx
+++ src/lime/system/Clipboard.hx
@@ -2,57 +2,17 @@ package lime.system;
 
 
 import lime.app.Application;
-import lime.app.Event;
-
-#if flash
-import flash.desktop.Clipboard in FlashClipboard;
-#elseif (js && html5)
-import lime._backend.html5.HTML5Window;
-#end
-
-#if !lime_debug
-@:fileXml('tags="haxe,release"')
-@:noDebug
-#end
 
 @:access(lime.ui.Window)
-
-
 class Clipboard {
 	
 	
-	public static var onUpdate = new Event<Void->Void> ();
 	public static var text (get, set):String;
 	
 	private static var _text:String;
 	
 	
-	
-	private static function __update ():Void {
-		
-		var cacheText = _text;
-		
-		if (_text != cacheText) {
-			
-			onUpdate.dispatch ();
-			
-		}
-		
-	}
-	
-	
-	
-	
-	// Get & Set Methods
-	
-	
-	
-	
-	private static function get_text ():String {
-		
-		#if flash
-		__update ();
-		#end
+	private static inline function get_text ():String {
 		
 		return _text;
 		
@@ -70,25 +30,16 @@ class Clipboard {
 	
 	public static function setText (value:String, syncSystemClipboard:Bool) {
 		
-		var cacheText = _text;
 		_text = value;
 		
 		if (syncSystemClipboard) {
 			
-			#if (js && html5)
 			var window = Application.current.window;
 			if (window != null) {
 				
 				window.backend.setClipboard (value);
 				
 			}
-			#end
-			
-		}
-		
-		if (_text != cacheText) {
-			
-			onUpdate.dispatch ();
 			
 		}
 		
