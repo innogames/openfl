c01e1298015cfeaacf61459a79295ecd7f3ca44d
Dan Korostelev <nadako@gmail.com>
2019-10-22 22:39:51 +0200

    merge HTML5Mouse into Mouse

--- src/lime/ui/Mouse.hx
+++ src/lime/ui/Mouse.hx
@@ -1,23 +1,58 @@
 package lime.ui;
 
-import lime._backend.html5.HTML5Mouse as MouseBackend;
+import lime.app.Application;
 
+@:access(lime.app.Application)
+@:access(lime.ui.Window)
 class Mouse {
 	public static var cursor(get, set):MouseCursor;
 
+	static var __cursor:MouseCursor;
+	static var __hidden:Bool;
+
 	public static function hide():Void {
-		MouseBackend.hide();
+		if (!__hidden) {
+			__hidden = true;
+			Application.current.window.backend.element.style.cursor = "none";
+		}
 	}
 
 	public static function show():Void {
-		MouseBackend.show();
+		if (__hidden) {
+			__hidden = false;
+
+			var cacheValue = __cursor;
+			__cursor = null;
+			set_cursor(cacheValue);
+		}
 	}
 
 	static function get_cursor():MouseCursor {
-		return MouseBackend.get_cursor();
+		if (__cursor == null)
+			return DEFAULT;
+		return __cursor;
 	}
 
 	static function set_cursor(value:MouseCursor):MouseCursor {
-		return MouseBackend.set_cursor(value);
+		if (__cursor != value) {
+			__cursor = value;
+			if (!__hidden) {
+				Application.current.window.backend.element.style.cursor = switch (value) {
+					case ARROW: "default";
+					case CROSSHAIR: "crosshair";
+					case MOVE: "move";
+					case POINTER: "pointer";
+					case RESIZE_NESW: "nesw-resize";
+					case RESIZE_NS: "ns-resize";
+					case RESIZE_NWSE: "nwse-resize";
+					case RESIZE_WE: "ew-resize";
+					case TEXT: "text";
+					case WAIT: "wait";
+					case WAIT_ARROW: "wait";
+					default: "auto";
+				}
+			}
+		}
+		return value;
 	}
 }
