3b69c03aeec88b3871da02f0a1f25e4038cabe09
Dan Korostelev <nadako@gmail.com>
2020-03-26 03:48:45 +0100

    fix compilation with latest Haxe 4.1

--- src/openfl/display/Stage.hx
+++ src/openfl/display/Stage.hx
@@ -871,7 +871,11 @@ class Stage extends DisplayObjectContainer {
 			neko.Lib.rethrow (e);
 			#elseif js
 			try {
+				#if (haxe < version("4.1.0-rc.1"))
 				var exc = @:privateAccess haxe.CallStack.lastException;
+				#else
+				var exc = @:privateAccess haxe.NativeStackTrace.lastError;
+				#end
 				if (exc != null && Reflect.hasField (exc, "stack") && exc.stack != null && exc.stack != "") {
 					js.Browser.console.log(exc.stack);
 					e.stack = exc.stack;
