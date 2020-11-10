23fab217953a6bf1ea5d069cd48d99c5155858cb
Nenad Bojkovski <nenad.bojkovski@innogames.com>
2020-11-10 12:50:23 +0100

    Remove SKIP major performance caveat test as we want it always to happen

--- src/lime/graphics/Renderer.hx
+++ src/lime/graphics/Renderer.hx
@@ -55,15 +55,13 @@ class Renderer {
 			return;
 		}
 
-		var test:MajorPerformanceCaveatTest = Reflect.hasField(window.config, "majorPerformanceCaveatTest") ?
-			window.config.majorPerformanceCaveatTest : SKIP;
+		var failIfMajorPerformanceCaveat = Reflect.hasField(window.config, "failIfMajorPerformanceCaveat") ?
+			window.config.failIfMajorPerformanceCaveat : false;
 
-		if (test != SKIP) {
-			createGLContext(true);
-			hasMajorPerformanceCaveat = context == null; // true if hardware context fails
-		}
+		createGLContext(true);
+		failIfMajorPerformanceCaveat = context == null; // true if hardware context fails
 
-		if (context == null && test != FAIL_IF) {
+		if (context == null && !failIfMajorPerformanceCaveat) {
 			createGLContext(false);
 		}
 	}
