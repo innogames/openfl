040dbdc73bef30f858222afb235c90ffaed8ed9b
Nenad Bojkovski <nenad.bojkovski@innogames.com>
2022-08-26 15:23:22 +0200

    Add backbuffer alpha switch and powerPrefence setting

--- src/lime/graphics/Renderer.hx
+++ src/lime/graphics/Renderer.hx
@@ -30,15 +30,17 @@ class Renderer {
 	private function createGLContext(failIfMajorPerformanceCaveat:Bool):Void {
 		var transparentBackground = Reflect.hasField(window.config, "background") && window.config.background == null;
 		var colorDepth = Reflect.hasField(window.config, "colorDepth") ? window.config.colorDepth : 16;
+		var alpha = Reflect.hasField(window.config, "alpha") ? window.config.alpha : false;
 
 		var options = {
-			alpha: (transparentBackground || colorDepth > 16) ? true : false,
+			alpha: (alpha || transparentBackground || colorDepth > 16) ? true : false,
 			antialias: Reflect.hasField(window.config, "antialiasing") ? window.config.antialiasing > 0 : false,
 			depth: Reflect.hasField(window.config, "depthBuffer") ? window.config.depthBuffer : true,
 			premultipliedAlpha: true,
 			stencil: Reflect.hasField(window.config, "stencilBuffer") ? window.config.stencilBuffer : false,
 			preserveDrawingBuffer: false,
-			failIfMajorPerformanceCaveat: failIfMajorPerformanceCaveat
+			failIfMajorPerformanceCaveat: failIfMajorPerformanceCaveat,
+			powerPreference: Reflect.hasField(window.config, "powerPreference") ? window.config.powerPreference.toString() : PowerPreferance.DEFAULT.toString(),
 		};
 
 		for (name in ["webgl2", "webgl", "experimental-webgl"]) {
